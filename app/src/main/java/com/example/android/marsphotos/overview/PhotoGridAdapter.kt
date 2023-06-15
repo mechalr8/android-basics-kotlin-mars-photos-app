7ff5e2b59000bb31477c78ce44d4d0288a66c4c7
mechalr8 <harsitagrawal59@gmail.com>
2023-06-15 18:30:15 +0530

    First Commit

--- app/src/main/java/com/example/android/marsphotos/overview/PhotoGridAdapter.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/PhotoGridAdapter.kt
@@ -1,8 +1,9 @@
 package com.example.android.marsphotos.overview
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ListAdapter
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.marsphotos.databinding.GridViewItemBinding
 import com.example.android.marsphotos.network.MarsPhoto
@@ -11,29 +12,32 @@ class PhotoGridAdapter : ListAdapter<MarsPhoto,
         PhotoGridAdapter.MarsPhotoViewHolder>(DiffCallback) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoGridAdapter.MarsPhotoViewHolder {
-        TODO("Not yet implemented")
+        return MarsPhotoViewHolder(GridViewItemBinding.inflate(
+            LayoutInflater.from(parent.context)
+        ))
     }
 
     override fun onBindViewHolder(holder: PhotoGridAdapter.MarsPhotoViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val marsPhoto = getItem(position)
+        holder.bind(marsPhoto)
     }
 
     class MarsPhotoViewHolder(private var binding :
                               GridViewItemBinding) :
         RecyclerView.ViewHolder(binding.root){
-        fun bind(MarsPhoto: MarsPhoto){
-            binding.photo= MarsPhoto
+        fun bind(marsPhoto: MarsPhoto){
+            binding.photo= marsPhoto
             binding.executePendingBindings()
         }
     }
 
     companion object DiffCallback : DiffUtil.ItemCallback<MarsPhoto>() {
         override fun areItemsTheSame(oldItem: MarsPhoto, newItem: MarsPhoto): Boolean {
-            TODO("Not yet implemented")
+            return oldItem.id == newItem.id
         }
 
         override fun areContentsTheSame(oldItem: MarsPhoto, newItem: MarsPhoto): Boolean {
-            TODO("Not yet implemented")
+            return oldItem.imgSrcUrl == newItem.imgSrcUrl
         }
     }
 }
